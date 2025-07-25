9491fe54069944bc2bdb242e157b0f6e347c5cb4
Olha Karpenko <olga8karp@gmail.com>
2025-07-25 19:00:38 +0200

    Add loading of questions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import Header from "./Header";
 import Main from "./Main";
+import React, { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
 
+const initialState = {
+    questions: [],
+    status: 'loading',
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: 'ready',
+            }
+        case 'dataFailed':
+            return {
+                ...state,
+                status: 'error',
+            }
+        default:
+           throw new Error(`Unknown action type ${action.type}`);
+    }
+}
 function App() {
+    const [{ questions, status}, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+        .then((response) => response.json())
+        .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+        .catch(() => dispatch({ type: 'dataFailed' }));
+  }, []);
   return (
     <div className="app">
       <Header />
 
       <Main>
-          <p>1/15</p>
-          <p>Question?</p>
+          {status === 'loading' && <Loader />}
+          {status === 'error' && <Error />}
       </Main>
     </div>
   );
