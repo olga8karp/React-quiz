4cd670271e5f4775db8b5705a5e255a9b0e3760b
Olha Karpenko <olga8karp@gmail.com>
2025-07-28 11:04:05 +0200

    Add CSS classes

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,9 +1,9 @@
 function StartScreen({ numOfQuestions, dispatch }) {
     return (
-        <div>
+        <div className="start">
            <h2>Welcome to the React Quizz!</h2>
            <h3>{numOfQuestions} question to test your React mastery</h3>
-           <button onClick={() => dispatch({ type: 'start' })}>Lets start</button>
+           <button className="btn btn-ui" onClick={() => dispatch({ type: 'start' })}>Lets start</button>
         </div>
     )
 }
